e62cec72d3ca6a623477917c915d979c5ddb5a51
mhchang1024 <mhchang1024@hotmail.com>
2023-06-17 19:23:43 -0700

    completed the app

--- WeatherAppProject/WeatherAppProject/views/LoadingView.swift
+++ WeatherAppProject/WeatherAppProject/views/LoadingView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .white)).frame(maxWidth: .infinity, maxHeight: .infinity)
+        ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             
     }
 }
